1c40789e923cde0c0d1fbfcafafc9af0f8bf63e2
Juan Pedro Monetta Sanchez <jmonetta@debianLaptop>
2022-12-15 10:00:18 -0300

    New stuff to the Clojure utils

--- clojure.el
+++ clojure.el
@@ -16,21 +16,34 @@
 		 ,hy-cmd-title
 		 ,@hy-triplets))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; FlowStorm dev menu ;;
+;;;;;;;;;;;;;;;;;;;;;;;;
 
 (cider-project-hydra
 
  flow-storm-dev-menu "FlowStrom dev menu"
  
- ("s" flow-storm-stop          "Stop"       "(dev/stop)")
+ ("s" flow-storm-stop         "Stop"       "(dev/stop)")
  ("l" flow-storm-start-local  "Run local"  "(dev/start-local)")
  ("r" flow-storm-start-remote "Run remote" "(dev/start-remote)")
  ("R" flow-storm-refresh      "Refresh"    "(dev/refresh)"))
 
+;;;;;;;;;;;;;;;;;;;;;
+;; Hansel dev menu ;;
+;;;;;;;;;;;;;;;;;;;;;
+
 (cider-project-hydra
 
- hansel-dev-menu "FlowStrom dev menu"
+ hansel-dev-menu "Hansel dev menu"
  
  ("R" hansel-refresh "Refresh" "(dev/refresh)"))
+
+
+
+;;;;;;;;;;;;;;;;;;;;
+;; Menu utilities ;;
+;;;;;;;;;;;;;;;;;;;;
 
 (defvar clojure-dev-menu-name nil)
 
@@ -41,3 +54,11 @@
 
 (define-key clojure-mode-map (kbd "<f5>") 'clojure-dev-menu-show)
 (define-key cider-repl-mode-map (kbd "<f5>") 'clojure-dev-menu-show)
+
+(defun cider-tap-last-and-show-debugger ()
+  (interactive)
+
+  (cider-interactive-eval "(tap> *1)")
+  (shell-command "i3-msg '[title=\"Flowstorm debugger\"] scratchpad show'"))
+
+(define-key clojure-mode-map (kbd "<f6>") 'cider-tap-last-and-show-debugger)
